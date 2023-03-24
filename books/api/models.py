9fddb89e69031f40fbbd78721326fce82fa346c8
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-24 11:17:36 +0100

    "commit"

--- books/api/models.py
+++ books/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=250)
+    number_pages = models.IntegerField()
+    quantity = models.PositiveIntegerField()
+    publish_date = models.DateField()
+
