d0a05cc5e238c581164d5524bb2d2c252ad153dc
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-24 18:10:36 +0100

    "commit"

--- books/api/views.py
+++ books/api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Book
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view,APIView
+from .serializers import BookSerializer
+from rest_framework.parsers import JSONParser
 # Create your views here.
 
 def Book_list(request):
@@ -9,4 +14,20 @@ def Book_list(request):
     return JsonResponse({
         'books':book_value
     })
+
+
+@api_view(['GET'])
+def book_list(request):
+    if request.method == 'GET':
+        book = Book.objects.all()
+        serializers = BookSerializer(book,many=True)
+        return Response(serializers.data)
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializers = BookSerializer(data=data)
+        if serializers.is_valid():
+            serializers
+
+
+
 
