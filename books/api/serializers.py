b8aaa05eddafc67945e6359bab564db6e3b4bc4d
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-25 15:10:23 +0100

    "commit"

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from .models import Book
+from rest_framework.validators import ValidationError
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+
+    def validate_name(self, attrs):
+        if attrs == 'Game of Madness':
+            message = 'This book is not allowed'
+            raise ValidationError(message)
+        return attrs
+
