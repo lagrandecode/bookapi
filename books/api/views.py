894095af3c1b9940ea86e1cbb144bd9e69f0cbb7
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-25 14:09:27 +0100

    "commit"

--- books/api/views.py
+++ books/api/views.py
@@ -8,48 +8,84 @@ from .serializers import BookSerializer
 from rest_framework.parsers import JSONParser
 # Create your views here.
 
-def Book_list(request):
-    books = Book.objects.all()
-    book_value = list(books.values())
-    return JsonResponse({
-        'books':book_value
-    })
+# def Book_list(request):
+#     books = Book.objects.all()
+#     book_value = list(books.values())
+#     return JsonResponse({
+#         'books':book_value
+#     })
 
 
-@api_view(['GET','POST'])
-def book_list(request):
-    if request.method == 'GET':
-        book = Book.objects.all()
-        serializers = BookSerializer(book,many=True)
-        return Response(serializers.data)
-    elif request.method == 'POST':
-        serializers = BookSerializer(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
+# @api_view(['GET','POST'])
+# def book_list(request):
+#     if request.method == 'GET':
+#         book = Book.objects.all()
+#         serializers = BookSerializer(book,many=True)
+#         return Response(serializers.data)
+#     elif request.method == 'POST':
+#         serializers = BookSerializer(data=request.data)
+#         if serializers.is_valid():
+#             serializers.save()
+#             return Response(serializers.data)
+#         return Response(serializers.errors)
+
+# @api_view(['GET','PUT','DELETE'])
+# def book_view(request,pk):
+#     try:
+#         books = Book.objects.get(pk=pk)
+#     except:
+#         Book.DoesNotExist
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
+#     if request.method == 'GET':
+#         serializers = BookSerializer(books)
+#         return Response(serializers.data)
+#     elif request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         serializers = BookSerializer(books,data)
+#         if serializers.is_valid():
+#             serializers.save()
+#             return Response(serializers.data)
+#         return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'DELETE':
+#         books.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+class BookList(APIView):
+    def get(self,request,format=None):
+        if request.method == 'GET':
+            books = Book.objects.all()
+            serializers = BookSerializer(books,many=True)
             return Response(serializers.data)
-        return Response(serializers.errors)
-
-@api_view(['GET','PUT','DELETE'])
-def book_view(request,pk):
-    try:
-        books = Book.objects.get(pk=pk)
-    except:
-        Book.DoesNotExist
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    if request.method == 'GET':
+    def post(self,request,format=None):
+        if request.method == 'POST':
+            serializers = BookSerializer(books,data=request.data)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data)
+            return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+class BookView(APIView):
+    def get_object(self,pk):
+        try: 
+            return Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    def get(self,request,pk,format=None):
+        books = self.get_object(pk)
         serializers = BookSerializer(books)
         return Response(serializers.data)
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializers = BookSerializer(books,data)
+    def put(self,request,pk,format=None):
+        books = self.get_object(pk)
+        serializers = BookSerializer(books,data=request.data)
         if serializers.is_valid():
             serializers.save()
             return Response(serializers.data)
         return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+    def delete(self,request,pk,format=None):
+        books = self.get_object(pk)
         books.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
-
 
 
